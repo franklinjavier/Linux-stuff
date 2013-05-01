b9c37ab4d3dfbe7f822599da74735abe8c3ab51e
Franklin Javier <franklinjalves@gmail.com>
2013-05-01 01:49:49 -0300

    Added mediterranean theme an faenza icon

--- init.sh
+++ init.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 sudo apt-get install php5 apache2 mysql-server mysql-client php5-mysql git-gui jackd gimp chromium-browser vim-gtk compizconfig-settings-manager vlc mixxx ttf-inconsolata exuberant-ctags gnome-shell curl python-pip
 
-" install the cool themes for unity "
-mkdir -p ~/Downloads/theme-unity
-cd ~/Downloads/theme-unity
-wget http://webupd8.googlecode.com/files/theme-unity_0.2.tar.gz
-tar -xvf theme-unity_0.2.tar.gz
-./theme-unity
-
-" Nautilus elementary "
-sudo add-apt-repository ppa:am-monkeyd/nautilus-elementary-ppa
-sudo apt-get update && sudo apt-get dist-upgrade
-
 " Configuration of Apache "
 sudo chmod 777 /var/www/ -R
 sudo a2enmod rewrite
 sudo sed -i 's/AllowOverride\ None/AllowOverride\ All/' /etc/apache2/sites-available/default
 sudo /etc/init.d/apache2 restart
 
-" Setting up Mediterranean theme and faenza icons"
+" Setting up Mediterranean theme and faenza/faience icons"
+" Nautilus elementary "
+sudo add-apt-repository ppa:am-monkeyd/nautilus-elementary-ppa
 sudo add-apt-repository ppa:noobslab/themes
 sudo add-apt-repository ppa:tiheum/equinox
-sudo apt-get update
-sudo apt-get install mediterranean-theme
-sudo apt-get install faenza-icon-theme
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get install mediterranean-theme faenza-icon-theme faience-icon-theme faenza-icon-theme
